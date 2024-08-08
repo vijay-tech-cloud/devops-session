83eb2fe8977a2f413741bf53a488520d0182e71c
onestepsolutions <info@onestepsolutions.co.in>
2024-08-09 03:24:25 +0530

    Formapp in java

---
+++ java-app/src/main/java/com/example/demoapp/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
